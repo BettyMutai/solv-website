4f52155644f531ac580f11c65948ff7ceb96ef79
Betty Mutai <bettymutai00@gmail.com>
2022-11-11 13:37:14 +0300

    Add missing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from "./about/about.component";
 import {MediaComponent} from "./media/media.component";
 import {ReachUsComponent} from "./reach-us/reach-us.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'news', component: MediaComponent },
   { path: 'reach-us', component: ReachUsComponent},
+  { path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
